3df21ef1ddfbfbd23fde5750430a20a29b43864d
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2018-05-01 14:36:47 +0900

    version up. change nuspec document.

--- Project/Nuget/MakeNupack.bat
+++ Project/Nuget/MakeNupack.bat
@@ -1,4 +1,4 @@
 rd /s /q "../ReleaseBinary"
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "../Codeer.Friendly/Codeer.Friendly.sln" /rebuild Release
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "../Codeer.Friendly/Codeer.Friendly.sln" /rebuild Release-Eng
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" "../Codeer.Friendly/Codeer.Friendly.sln" /rebuild Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" "../Codeer.Friendly/Codeer.Friendly.sln" /rebuild Release-Eng
 nuget pack friendly.nuspec
